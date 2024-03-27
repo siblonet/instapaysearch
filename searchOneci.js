1e069c3733ba4b90c9b2a1b07f9f9516e2e528c9
John Kokar <princedibloni4@gmail.com>
2024-03-27 12:45:39 +0000

    dojnff

--- searchOneci.js
+++ searchOneci.js
@@ -3,8 +3,11 @@ const Search = async () => {
 
     const nni = document.getElementById('nni').value;
 
-    if (nni, token) {
+    if (nni && nni.length > 5 && token) {
         document.getElementById('valideuser').style.display = "none"
+
+        document.getElementById('messago').innerHTML = `<img src="./asserts/loaging.gif" style="height: 100%; width: 100%;"
+        alt="Loading ...">`;
         document.getElementById('messago').style.display = "block"
 
         document.getElementById('nni').blur();
@@ -12,7 +15,8 @@ const Search = async () => {
         const response = await requesttoBackendAtrubi('GET', `${rnppUrl}/oneci/persons/${nni}?attributeNames=FIRST_NAME&attributeNames=LAST_NAME&attributeNames=BIRTH_DATE&attributeNames=GENDER`, token);
 
         if (response) {
-            document.getElementById('messago').innerHTML = `<img src="./asserts/loaging.gif" style="height: 100%; width: 100%;"`;
+            document.getElementById('messago').innerHTML = `<img src="./asserts/loaging.gif" style="height: 100%; width: 100%;"
+            alt="Loading ...">`;
             document.getElementById('messago').style.display = "none"
             //console.log(response);
 
@@ -27,12 +31,13 @@ const Search = async () => {
 
         } else {
 
-            document.getElementById('messago').innerHTML = `<p style="color: red; font-weight: bold;">${nni} Introuvable!</p>`;
+            document.getElementById('messago').innerHTML = `<p style="color: red; font-weight: bold; text-align: center; margin-top: 15%; font-size: 30px !important;">${nni} Introuvable!</p>`;
 
             setTimeout(() => {
-                document.getElementById('messago').innerHTML = `<img src="./asserts/loaging.gif" style="height: 100%; width: 100%;"`;
+                document.getElementById('messago').innerHTML = `<img src="./asserts/loaging.gif" style="height: 100%; width: 100%;"
+                alt="Loading ...">`;
                 document.getElementById('messago').style.display = "none"
-            }, 7000);
+            }, 1000);
         }
     }
 }
@@ -43,3 +48,16 @@ nniA.addEventListener("input", function () {
     nniA.value = nniA.value.replace(/\s/g, '')
 
 });
+
+const Indiponible = () => {
+    document.getElementById('messago').style.display = "block"
+    const messagso = `<p style="color: red; font-weight: bold; text-align: center; margin-top: 15%; font-size: 30px !important;">Cette fonctionnalité est indisponible</p>`
+    document.getElementById('messago').innerHTML = messagso;
+
+    setTimeout(() => {
+        document.getElementById('messago').innerHTML = `<img src="./asserts/loaging.gif" style="height: 100%; width: 100%;"
+        alt="Loading ...">`;
+        document.getElementById('messago').style.display = "none"
+    }, 1000);
+
+}
