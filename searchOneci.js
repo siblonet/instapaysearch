3d3f6063264bd0dd180d5849270fb98cb643ccd7
John Kokar <princedibloni4@gmail.com>
2024-03-27 02:30:16 +0000

    fraa

--- searchOneci.js
+++ searchOneci.js
@@ -8,16 +8,22 @@ const Search = async () => {
 
         document.getElementById('nni').blur();
 
-        const response = await requesttoBackendAtrubi('GET', `${rnppUrl}/oneci/persons/${nni}?attributeNames=FIRST_NAME&attributeNames=LAST_NAME&attributeNames=BIRTH_DATE&attributeNames=GENDER&attributeNames=BIRTH_COUNTRY`, token);
+        const response = await requesttoBackendAtrubi('GET', `${rnppUrl}/oneci/persons/${nni}?attributeNames=FIRST_NAME&attributeNames=LAST_NAME&attributeNames=BIRTH_DATE&attributeNames=GENDER`, token);
 
         if (response) {
-            document.getElementById('messago').innerHTML = `<p style="color: #028a61; font-weight: bold;">${nni} est valide !</p>`;
-            console.log(response);
-            
-            setTimeout(() => {
-                document.getElementById('messago').innerHTML = `<img src="./asserts/loaging.gif" style="height: 100%; width: 100%;"`;
-                document.getElementById('messago').style.display = "none"
-            }, 7000);
+            document.getElementById('messago').innerHTML = `<img src="./asserts/loaging.gif" style="height: 100%; width: 100%;"`;
+            document.getElementById('messago').style.display = "none"
+            //console.log(response);
+
+            const userinfos = `
+                                <p>Nom: ${response.LAST_NAME}</p>
+                                <p>Prénom: ${response.FIRST_NAME}</p>
+                                <p>Date de naissance: ${response.BIRTH_DATE}</p>
+                                <p>Genre: ${response.GENDER}</p>
+            `;
+            document.getElementById('valideuser').style.display = "flex"
+            document.getElementById('userinfos').innerHTML = userinfos;
+
         } else {
 
             document.getElementById('messago').innerHTML = `<p style="color: red; font-weight: bold;">${nni} Introuvable!</p>`;
